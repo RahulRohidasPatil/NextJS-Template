ccb00e3a2bf3328a128dacdf32f16d049d9f82da
RahulPatil <rahulrohidaspatil@gmail.com>
2026-03-22 14:06:55 +0100

    integrate Dark Mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next"
+import { ThemeProvider } from "next-themes"
 import { Geist, Geist_Mono } from "next/font/google"
 import "./globals.css"
 
@@ -22,8 +23,18 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
     <html
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      suppressHydrationWarning
     >
-      <body className="flex min-h-full flex-col">{children}</body>
+      <body className="flex min-h-full flex-col">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
+import { ModeToggle } from "@/components/mode-toggle"
+
 export default function Home() {
   return (
-    <main className="mt-2 flex flex-col items-center">
+    <main className="my-auto flex flex-col items-center gap-2">
+      <ModeToggle align="center" />
       <h1 className="text-3xl leading-10 font-semibold tracking-tight">
         Next.js Template by Rahul Patil
       </h1>
